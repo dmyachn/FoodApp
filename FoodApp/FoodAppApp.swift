527301197bc3a88078a0471b291c4f10695cc396
dmyachn <d.myachn@gmail.com>
2021-09-02 20:44:46 +0300

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by dmitry on 02.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
